75c43fcc224c524cf50bb4bbe023af4588994e31
fmcfar200 <FMCFAR200@caledonian.ac.uk>
2017-02-20 12:08:27 +0000

    commit

--- Lab1/Mesh.h
+++ Lab1/Mesh.h
@@ -1,47 +1,57 @@
 #pragma once
 #include <glm\glm.hpp>
 #include <GL\glew.h>
-#include "vector"
+#include <string>
+#include "obj_loader.h"
 
-class Vertex
+struct Vertex
 {
 public:
 	Vertex(const glm::vec3& pos, const glm::vec2& texCoord)
 	{
 		this->pos = pos;
 		this->texCoord = texCoord;
+		this->normal = normal;
 	}
 
+	glm::vec3* GetPos() { return &pos; }
+	glm::vec2* GetTexCoord() { return &texCoord; }
+	glm::vec3* GetNormal() { return &normal; }
+
+private:
 	glm::vec3 pos;
 	glm::vec2 texCoord;
-
-protected:
-private:
-	
+	glm::vec3 normal;
 };
 
 class Mesh
 {
 public:
-	Mesh(Vertex* vertices, unsigned int numVertices);
+	Mesh();
 	~Mesh();
+
 	void Draw();
+	void init(Vertex* vertices, unsigned int numVertices, unsigned int* indices, unsigned int numIndices);
+	void loadModel(const std::string& filename);
+	void initModel(const IndexedModel& model);
+
+	void loadModel(const std::string filename);
 
 private:
+
+
 
 	enum
 	{
 		POSITION_VERTEXBUFFER,
 		TEXCOORD_VB,
-		NUM_BUFFER
+		NORMAL_VB,
+		INDEX_VB,
+		NUM_BUFFERS
 	};
 
 	GLuint vertexArrayObject;
-	GLuint vertexArrayBuffers[NUM_BUFFER];//creates our areray of buffers
+	GLuint vertexArrayBuffers[NUM_BUFFERS]; // create our array of buffers
 	unsigned int drawCount; //how much of the vertexArrayObject do we want to draw
-
-
-
-
 };
 
