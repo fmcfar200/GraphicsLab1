bf0f83f814adac471480124463cbd0e99bcf0a3a
fmcfar200 <fmcfar200@caledonian.ac.uk>
2017-03-20 19:07:13 +0000

    Start

--- CourseworkStarter/CourseworkStarter/Game.cpp
+++ CourseworkStarter/CourseworkStarter/Game.cpp
@@ -5,6 +5,9 @@
 unsigned int inds[] = { 0, 1, 2 };
 Transform trans;
 
+//CHANGE 
+string RESOURCE_PATH = "C:\\Users\\Fraser\\Documents\\Uni\\Year3\\B\\Graphics\\GraphicsLabs\\CourseworkStarter\\res\\";
+
 Game::Game()
 {
 	gameState = State::PLAY;
@@ -31,8 +34,9 @@ void Game::RunGame()
 void Game::initialiseSystems()
 {
 	gameDisplay.initialiseDisplay();
-	//CHANGE FILE PATHS BETWEEN UNI AND HOME!!!!!!!!!!!!!!!!!!!!!!!!
-	mesh1.loadModelFromFile("C:\\Users\\fmcfar200\\Desktop\\GitHub\\GraphicsLabs\\CourseworkStarter\\res\\monkey3.obj");
+	mesh1.loadModelFromFile(RESOURCE_PATH + "monkey3.obj");
+	mesh2.loadModelFromFile(RESOURCE_PATH + "monkey3.obj");
+
 	camera.initialiseCamera(glm::vec3(0, 0, -5), 70.0f, (float)gameDisplay.GetWidth() / gameDisplay.GetHeight(), 0.01f, 1000.0);
 }
 
@@ -89,18 +93,21 @@ void Game::draw()
 
 	
 	gameDisplay.ClearDisplay(0.0f,0.0f,0.0f,1.0f);
-	//CHANGE FILE PATHS BETWEEN UNI AND HOME!!!!!!!!!!!!!!!!!!!!!!!!
-	ShaderManager shader("C:\\Users\\fmcfar200\\Desktop\\GitHub\\GraphicsLabs\\CourseworkStarter\\res\\"); //shader obj
-	TextureManager texture("C:\\Users\\fmcfar200\\Desktop\\GitHub\\GraphicsLabs\\CourseworkStarter\\res\\bricks.jpg");
+	ShaderManager shader(RESOURCE_PATH); //shader obj
+	TextureManager texture(RESOURCE_PATH + "bricks.jpg");
 
 	trans.SetPos(glm::vec3(0, 0.0, 0.0));
-	trans.SetRot(glm::vec3(0.0, 0.0,0));
-	trans.SetScale(glm::vec3(1,1,1));
+	trans.SetRot(glm::vec3(0.0, 0.0, 0));
+	trans.SetScale(glm::vec3(1, 1, 1));
+
 
 	shader.BindShader();//bind shader
 	shader.Update(trans,camera);
+
 	texture.BindTexture(0);
+
 	mesh1.DrawMesh();//draws mesh
+
 	count = count + 0.01f;
 
 	glEnableClientState(GL_COLOR_ARRAY);
